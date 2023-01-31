2d2b9cba34e24c307f0af6e56ee99c58d0644c16
zkl2333 <i@zkl2333.com>
2023-01-31 20:18:11 +0800

    ci: 🎡 修复ci

--- scripts/ci-version.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../', 'package.json'), 'utf8'));
-const version = packageJson.version;
-
-console.log(version);
